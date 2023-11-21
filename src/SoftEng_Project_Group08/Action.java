703d4ee8f7c39a3b10a50f91d3bb04f5eebb6082
MarioDC19 <m.dellacorte19@studenti.unisa.it>
2023-11-21 17:03:13 +0100

    Implementazione interfacce "Action" e "Trigger"

---
+++ src/SoftEng_Project_Group08/Action.java
@@ -0,0 +1,11 @@
+package SoftEng_Project_Group08;
+
+/**
+ *
+ * @author mario
+ */
+public interface Action {
+
+    public void execute();
+
+}
---
+++ src/SoftEng_Project_Group08/Trigger.java
@@ -0,0 +1,11 @@
+package SoftEng_Project_Group08;
+
+/**
+ *
+ * @author mario
+ */
+public interface Trigger {
+
+    public boolean check();
+
+}
