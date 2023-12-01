6dd354593366b10a04df618b6c9aea4750bd13e2
mauri <mauri@Maurizio.homenet.telecomitalia.it>
2023-12-01 12:41:36 +0100

    implementazione Model per l'azione DeleteFileAction implementazione test DeleteFileActionTest

---
+++ test/model_tests/DeleteFileActionTest.java
@@ -0,0 +1,40 @@
+
+package model_tests;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import softeng_project_group08.model.DeleteFileAction;
+
+/**
+ *
+ * @author group08
+ */
+public class DeleteFileActionTest {
+    
+    private static final String FILE_PATH = "testSource.txt";
+    
+    private File sourceFile;
+    
+    @Before
+    public void setUp() throws IOException {
+        sourceFile = new File(FILE_PATH);
+        sourceFile.createNewFile();
+    }
+    
+    @After
+    public void tearDown(){
+        sourceFile.delete();
+    }
+    
+    @Test
+    public void testDeleteFileSuccess(){
+        DeleteFileAction deleteAction = new DeleteFileAction(sourceFile);
+        deleteAction.execute();
+        
+        assertFalse(sourceFile.exists());
+    }  
+}
