0401294ccab13fdc8af056cb9e6d4ae7b947adc0
mauri <mauri@Maurizio.homenet.telecomitalia.it>
2023-12-05 19:02:15 +0100

    inserimento Test JUnit per la classe DayOfWeekTrigger

---
+++ test/model_tests/DayOfWeekTriggerTest.java
@@ -0,0 +1,36 @@
+package model_tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import softeng_project_group08.model.DayOfWeekTrigger;
+
+/**
+ *
+ * @author group08
+ */
+public class DayOfWeekTriggerTest {
+
+    private DayOfWeekTrigger a, b;
+
+    @Before
+    public void setUp() {
+        // Test case a: trigger on the current day
+        a = new DayOfWeekTrigger(LocalDate.now().getDayOfWeek());
+
+        // Test case b: trigger on the next day
+        b = new DayOfWeekTrigger(LocalDate.now().plusDays(1).getDayOfWeek());
+    }
+
+    @Test
+    public void testCheck() {
+        // Test case a: should be true as it triggers on the current day
+        assertEquals(true, a.check());
+
+        // Test case b: should be false as it triggers on the next day
+        assertEquals(false, b.check());
+    }
+}
+
